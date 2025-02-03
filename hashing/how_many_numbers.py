9e1a5204995a3e603996032c03a6547d064b3e6d
DaviAieta <aieta.davi@gmail.com>
2025-02-02 18:29:09 -0700

    add hashing

---
+++ hashing/how_many_numbers.py
@@ -0,0 +1,33 @@
+# Task: You are given a list of numbers. How many distinct numbers does it contain?
+import time
+import random
+
+# as a list
+def count_distinct_list(numbers):
+    seen = []
+    for number in numbers:
+        if number not in seen:
+            seen.append(number)
+    return len(seen)
+
+def count_distinct_set(numbers):
+    seen = set()
+    for number in numbers:
+        if number not in seen:
+            seen.add(number)
+    return len(seen)
+
+# count_distinct_list VS. count_distinct_set
+# This change has a big effect on the efficiency of the algorithm.
+large_list = [random.randint(0, 10000) for _ in range(1_000_000)]
+
+start_time = time.time()
+print("Distinct (list):", count_distinct_list(large_list))
+end_time = time.time()
+print("Time (list):", round(end_time - start_time, 2), "s")
+
+# Testando a função com set
+start_time = time.time()
+print("Distinct (set):", count_distinct_set(large_list))
+end_time = time.time()
+print("Time (set):", round(end_time - start_time, 2), "s")
---
+++ hashing/set.py
@@ -0,0 +1,15 @@
+# The Python data structure set, based on hashing, maintains a set of elements.
+# The operations on the data structure include:
+
+numbers = set([1,2,3,4,5,6])
+
+# add
+numbers.add(7)
+
+# in that returns True or False
+print(8 in numbers)
+
+# remove
+numbers.remove(4)
+
+print(numbers[3])
